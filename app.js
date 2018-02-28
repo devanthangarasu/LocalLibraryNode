6d1d6ec1bc27f8f9099973bbfb87ebbbee28396f
karthiyayini <karthiyayini@cogzidel.com>
2018-02-28 16:28:47 +0530

    added

--- app.js
+++ app.js
@@ -102,11 +102,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-// Set Port
-app.set('port', (process.env.PORT || 3001));
-
-app.listen(app.get('port'), function(){
-	console.log('Server started on port '+app.get('port'));
-});
 
 module.exports = app;
